998bb7cea7f8919e2d5d99af97b8cc72ba2849bd
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-07-29 13:30:17 -0400

    working code.

--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 abstract class Book {
     String title;
@@ -15,21 +15,37 @@ abstract class Book {
 // Declare your class here. Do not use the 'public' access modifier.
 // Declare the price instance variable
 
-/**
- *   Class Constructor
- *
- *   @param title The book's title.
- *   @param author The book's author.
- *   @param price The book's price.
- **/
+
+class MyBook extends Book {
+    private int price;
+
+    /**
+     * Class Constructor
+     *
+     * @param title  The book's title.
+     * @param author The book's author.
+     * @param price  The book's price.
+     **/
 // Write your constructor here
 
-/**
- *   Method Name: display
- *
- *   Print the title, author, and price in the specified format.
- **/
+    MyBook(String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+
+    /**
+     * Method Name: display
+     * <p>
+     * Print the title, author, and price in the specified format.
+     **/
 // Write your method here
+    @Override
+    void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+}
 
 // End class
 
